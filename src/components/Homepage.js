0d5df92ae49cf1c144557f664cddfff659079634
Yash Chaudhari <chaudhariyash10@gmail.com>
2021-02-08 13:19:55 +0530

    Initial Source Tree

---
+++ src/components/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Homepage() {
+    return (
+        <div>
+            {/* Info/ */}
+            {/* Video */}
+            {/* Login Button */}
+            {/* Footer */}
+
+        </div>
+    )
+}
+
+export default Homepage
