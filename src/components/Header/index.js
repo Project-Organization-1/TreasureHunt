d11a86e3300f759faa1a612d2c890b9e937383ab
Yash Chaudhari <chaudhariyash10@gmail.com>
2021-02-10 11:27:07 +0530

    Added routing back to Homepage And fixed styling of link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,15 +9,17 @@ function Header() {
         <Navbar bg="dark" variant="dark">
             <Container>
                 <Navbar.Brand>
+                    <Link to="/" className="header__title">
                     <img
                         id="logo"   
                         alt=""
                         src="/logos/VIlogo.jpg"
-                        width="30"
-                        height="30"
+                        width="40"
+                        height="40"
                         className="d-inline-block align-top"
                     />
                 Treasure Hunt
+                        </Link>
                 </Navbar.Brand>
                 <Nav.Item className="ml-auto">
                     <Link to="/login">
