5eaddd3ecdd5f3319b2e15e996846f8ae6b235fa
siddheshkothadi <realsiddheshko@gmail.com>
2021-02-12 13:45:57 +0530

    added console log for user

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
-function Login() { 
+function Login() {
 
   const [gruopId, setGruopId] = useState("");
   const [email, setEmail] = useState("");
@@ -8,42 +8,41 @@ function Login() {
   const handleClick = () => {
     submitUser(gruopId, email);
   }
-  function submitUser(groupId,email){
-        fetch(`http://localhost:5000/user/login/${groupId}/${email}`,
-        {
-            method: "GET",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin":true
-            },
-            mode: "cors",
-
-            // body: JSON.stringify({
-            //     groupId: groupId,
-            //     email: email,
-            // })
-        })
-        .then((res)=>{
-            // after adding the note, reload the page to reflect the changes
-            console.log("User Logged In");
-            console.log(res)
-        })
-        .catch(err => {
-            throw(err)
-        });
-    }
+  function submitUser(groupId, email) {
+    fetch(`http://localhost:5000/user/login/${groupId}/${email}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": true
+        },
+        mode: "cors"
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json();
+        }
+        throw new Error("Some error occurred while fetching database");
+      })
+      .then((resJson) => {
+        console.log(resJson);
+      })
+      .catch(err => {
+        throw (err)
+      });
+  }
 
   return (
     <section className="login">
       <div className="loginContainer">
         <h3>Log In</h3>
         <label>Group ID</label>
-        <input type="text" autoFocus required onChange={e => setGruopId(e.target.value)}/>
+        <input type="text" autoFocus required onChange={e => setGruopId(e.target.value)} />
         <label>Email</label>
         <input type="email" required onChange={e => setEmail(e.target.value)} />
         <div className="btnContainer">
-          <button onClick= {handleClick}>Sign In</button>
+          <button onClick={handleClick}>Sign In</button>
         </div>
       </div>
     </section>
