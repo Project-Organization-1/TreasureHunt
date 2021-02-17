cc3e7c0dda97b7024a4850533c4e274cf2021c19
Yash Chaudhari <chaudhariyash10@gmail.com>
2021-02-17 11:06:48 +0530

    Added Validations for sign in

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,54 +1,65 @@
-import React, { useState } from "react";
-import {Link} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 function Login() {
-
-  const [gruopId, setGruopId] = useState("");
+  const [groupId, setGruopId] = useState("");
   const [email, setEmail] = useState("");
+  const [status, setStatus] = useState(false);
+  const [valid, setValid] = useState(false);
 
-  const handleClick = () => {
-    submitUser(gruopId, email);
-  }
-
-  function submitUser(groupId, email) {
-    fetch(`http://localhost:5000/user/login/${groupId}/${email}`,
-      {
+  useEffect(() => {
+    function submitUser(groupId, email) {
+      fetch(`http://localhost:5000/user/login/${groupId}/${email}`, {
         method: "GET",
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": true
+          "Access-Control-Allow-Origin": true,
         },
-        mode: "cors"
+        mode: "cors",
       })
-      .then((res) => {
-        if (res.status === 200) {
-          return res.json();
-        }
-        throw new Error("Some error occurred while fetching database");
-      })
-      .then((resJson) => {
-        console.log(resJson);
-      })
-      .catch(err => {
-        throw (err)
-      });
-  }
+        .then((res) => {
+          if (res.status === 200) {
+            setValid(true);
+          } else {
+            setValid(false);
+          }
+          return setStatus(res.status);
+        })
+        .catch((err) => {
+          setStatus(err.status);
+          throw err;
+        });
+    }
 
-
+    submitUser(groupId, email);
+  }, [email, groupId]);
+  
   return (
     <section className="login">
       <div className="loginContainer">
         <h3>Log In</h3>
         <label>Group ID</label>
-        <input type="text" autoFocus required onChange={e => setGruopId(e.target.value)} />
+        <input
+          type="text"
+          autoFocus
+          required
+          onChange={(e) => setGruopId(e.target.value)}
+        />
+        {status === 201 && <p className="__warning">Group not Found</p>}
         <label>Email</label>
-        <input type="email" required onChange={e => setEmail(e.target.value)} />
+        <input
+          type="email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {status === 202 && <p className="__warning">User Not found</p>}
         <div className="btnContainer">
           <Link to="/level1">
-            <button onClick= {handleClick}>Sign In</button>
+            <button  disabled={!valid}>
+              Sign In
+            </button>
           </Link>
-
         </div>
       </div>
     </section>
